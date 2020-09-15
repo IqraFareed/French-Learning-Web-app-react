4012be7057a7d9ed0439dcfc728476d0f68edf2e
IqraFareed <iqrafareed56@gmail.com>
2020-09-15 13:33:04 +0500

    Updates

---
+++ precache-manifest.03c934bbb43fcc8ef75fa9c4c64f9a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec5dc3f0d7841f72177dd738f7fdbb2",
+    "url": "/French-Learning-Web-app-react/index.html"
+  },
+  {
+    "revision": "84f2eca34d8ae92bd076",
+    "url": "/French-Learning-Web-app-react/static/css/main.43686a37.chunk.css"
+  },
+  {
+    "revision": "c5e96a6a287cc60499f3",
+    "url": "/French-Learning-Web-app-react/static/js/2.1847e0ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/French-Learning-Web-app-react/static/js/2.1847e0ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f2eca34d8ae92bd076",
+    "url": "/French-Learning-Web-app-react/static/js/main.78ff781f.chunk.js"
+  },
+  {
+    "revision": "31990c7e1ef8c9c6a814",
+    "url": "/French-Learning-Web-app-react/static/js/runtime-main.2a8be78f.js"
+  }
+]);
